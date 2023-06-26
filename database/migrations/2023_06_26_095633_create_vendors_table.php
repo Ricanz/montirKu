898acc70ec432271d0f5148bd9a67a34c481f2ed
ctriyanti <riyanti@cashtree.id>
2023-06-26 17:53:59 +0700

    install package

---
+++ database/migrations/2023_06_26_095633_create_vendors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug');
+            $table->string('avatar');
+            $table->string('address');
+            $table->string('status');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
